840daff82216ee53090254d723b52e15e2392056
MeerimBat <merba971@gmail.com>
2025-10-22 15:24:58 +0200

    first commit

---
+++ W5 exercises.py
@@ -0,0 +1,12 @@
+primes=(2,3,5,7)
+for prime in primes:
+    print(prime)
+    print("-------")
+
+for x in range(23,27):
+    print(x)
+    
+count=0
+while count<5:
+    print(count)
+    count+=1
---
+++ W6 exercises.py
@@ -0,0 +1,6 @@
+phonebook={}
+phonebook["John"]=552233
+phonebook["Jack"]=552234
+phonebook["Jill"]=552235
+print(phonebook)
+
