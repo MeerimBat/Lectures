bb9f5abb4aa2a6cf8139651627d1a221f6aea9f3
MeerimBat <merba971@gmail.com>
2025-10-30 23:09:42 +0100

    hw draft

---
+++ Python lectures/Python HW/HW5.py
@@ -0,0 +1,12 @@
+import random
+
+print(random.randrange(1, 100))
+number_to_guess =(random.randrange(1, 100) )
+attempts = 0
+print ("A number has been generated. You have 3 attempts to guess it")
+guess=random.randrange
+if guess>number_to_guess:
+    print("Too high!")
+#Project: Number Guessing Game. The program chooses a secret random number between 1 and 100. The user has a limited number of attempts to guess it.
+#fter each guess, the program tells the user if their guess was "too high," "too low," or "correct."
+
