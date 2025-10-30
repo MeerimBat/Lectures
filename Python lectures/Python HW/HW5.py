f91364d23ce0ddcdd05c18307bef9cf0bdac2096
MeerimBat <merba971@gmail.com>
2025-10-30 23:14:18 +0100

    draft

--- Python lectures/Python HW/HW5.py
+++ Python lectures/Python HW/HW5.py
@@ -1,12 +1,20 @@
+# program chooses a secret random number between 1 and 100. The user has a limited number of attempts to guess it.#ter each guess, the program tells the user if their guess was "too high," "too low," or "correct."
 import random
-
-print(random.randrange(1, 100))
+def number_guessing():
+   print(random.randrange(1, 100))
 number_to_guess =(random.randrange(1, 100) )
-attempts = 0
+attempts = 3
 print ("A number has been generated. You have 3 attempts to guess it")
-guess=random.randrange
+
+guess = int(input("Take a guess: "))
+attempts += 1
 if guess>number_to_guess:
     print("Too high!")
-#Project: Number Guessing Game. The program chooses a secret random number between 1 and 100. The user has a limited number of attempts to guess it.
-#fter each guess, the program tells the user if their guess was "too high," "too low," or "correct."
+
+elif guess<number_to_guess:
+    print ("Too low")
+
+else: 
+    print("Correct")
+
 
