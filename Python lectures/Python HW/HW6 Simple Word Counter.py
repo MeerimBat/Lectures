d90d80caf97cfe0eb56a73813ee04a041e239982
MeerimBat <merba971@gmail.com>
2025-11-03 17:10:14 +0100

    updated hw

--- Python lectures/Python HW/HW6 Simple Word Counter.py
+++ Python lectures/Python HW/HW6 Simple Word Counter.py
@@ -1,11 +1,15 @@
 #Simple Word Counter. Write a program that asks the user for a sentence. The program should then count the occurrences of each word in the sentence
 #and display the result as a dictionary.
+import string 
+
 
 sentence=input("Write a sentence: ")
-word=sentence.lower().split()
-#word = sentence.translate(str.maketrans('', '', string.punctuation))
-#for word in sentence.lower().split():
-   #word=word.strip(" ,.,?!")
+
+translator = str.maketrans('', '', string.punctuation)
+word = sentence.translate(translator)
+word=word.lower().split() 
+print(word)
+
 count = {}
 
 for i in word:
