a8859783573da038c26794acf1862e06b4b8fdc7
MeerimBat <merba971@gmail.com>
2025-11-19 23:37:58 +0100

    hw 3ex

--- Python lectures/Python HW/HW7.py
+++ Python lectures/Python HW/HW7.py
@@ -1,3 +1,15 @@
+# A. match_ends
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
+def match_ends(strings):
+    count = 0
+    for s in strings:
+        if isinstance(s, str) and len(s) >= 2 and s[0] == s[-1]:
+            count += 1
+    return count
+print(match_ends(['aba','silly', 'lalal', 'sun', 'anna']))
 
 # B. front_x
 # Given a list of strings, return a list with the strings
