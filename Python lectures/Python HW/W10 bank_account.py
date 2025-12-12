22dc2204bb55aca2b6354f99d12e94d3ba90c3c1
MeerimBat <merba971@gmail.com>
2025-12-12 16:14:44 +0100

    hw w10

---
+++ Python lectures/Python HW/W10 bank_account.py
@@ -0,0 +1,46 @@
+class bank_account: 
+
+    def __init__(self, account_owner,balance,deposit, withdrawal,):
+        self.account_owner = account_owner
+        self.balance = balance
+        self.deposit = deposit
+        self.withdrawal = withdrawal
+
+    def deposit(self, amount):
+       if amount > 0:
+          self.balance += amount 
+          print(f"deposited $ {amount}. new_balance: $ {self.balance}")
+       else: 
+          print("Deposit amount must be positive! Don't try to cheat!")
+    
+    def withdrawal(self, amount):
+       if amount > 0:
+          if self.balance >= amount:
+             self.balance -= amount
+             print(f"withdrawal ${amount}. new_balance: $ {self.balance}")
+          else: 
+             print("Not enough funds, try some other ATM")
+                   
+    
+    #def new_balance(self):
+        
+    
+    def display_info(self):
+        print(f"{self.account_owner} {self.balance} {self.deposit} {self.withdrawal}")
+
+     
+#my_account = bank_account("Batman", 10000)
+#print("-" * 20)
+#my_account.deposit(2000)
+#my_account.withdrawal(5000)
+#my_account.display_info()
+#print("-" * 20) 
+
+#p1 = bank_account ("Batman",10000,2000,5000)
+#p2 = bank_account ("Spiderman",20000, 3000,4000)  
+#p3 = bank_account ("Catwoman", 50000, 5000, 20000) 
+
+#p1.display_info()
+#p2.display_info()
+#p3.display_info()
+                                             
