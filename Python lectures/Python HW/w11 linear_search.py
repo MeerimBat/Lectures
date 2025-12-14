ea27db360c3565b9cc14f7467f27c4ba13d2e94e
MeerimBat <merba971@gmail.com>
2025-12-14 20:12:26 +0100

    HW W11

---
+++ Python lectures/Python HW/w11 linear_search.py
@@ -0,0 +1,37 @@
+def linear_search(arr, target_value):
+    for i in range(len(arr)):
+        if arr[i] == target_value:
+           return i
+    return -1
+
+my_list =[25,30,35,40,45,50,55] 
+x = 50
+
+result = linear_search(my_list,x)
+
+if result != -1:
+   print ("The number is found", result)
+else:
+   print ("The numer is not found!")
+
+my_list_fruits =["banana","orange","kiwi","apple"] 
+x = "orange"
+
+result = linear_search(my_list_fruits,x)
+
+if result != -1:
+   print ("The number is found", result)
+else:
+   print ("The numer is not found!")
+
+my_list_birthyear =[1960, 1988,2003,2009,2015] 
+x = 2020
+
+result = linear_search(my_list_birthyear,x)
+
+if result != -1:
+   print ("The number is found", result)
+else:
+   print ("The numer is not found!")
+
+        
