87e80ef22cf9e39d7a73fe2f3a42a5accd0746a8
MeerimBat <merba971@gmail.com>
2025-12-22 23:41:57 +0100

    draft

--- Python lectures/Python HW/W12 leetcode.py
+++ Python lectures/Python HW/W12 leetcode.py
@@ -1,6 +1,6 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
-      nums.sort (nums)
-      return nums [nums.length/2]
+      #nums.sort (nums)
+      #return nums [nums.length/2]
 
 nums = [3,2,3]
